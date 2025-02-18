15eb62b8ea50f76424ed93c910270460bde3a351
Arthur <arthur-catrindadeunb@hotmail.com>
2025-02-17 22:44:21 -0300

    ajustando views

--- TF_B12_MiguelBarbosa_fimConsultas.sql
+++ TF_B12_MiguelBarbosa_fimConsultas.sql
@@ -21,45 +21,33 @@ USE beatport;
 -- Essa view ajuda a identificar quais artistas têm maior volume de lançamentos, 
 -- útil para analisar padrões de produtividade e relevância no mercado musical.
 CREATE VIEW ArtistAlbumCount AS
-SELECT a.artist_id, a.artist_name, COUNT(aa.album_id) AS total_albums
+SELECT a.artist_id, a.artist_name, COUNT(aa.fk_ALBUM_album_id) AS total_albums
 FROM ARTIST a
-LEFT JOIN ARTIST_ALBUM aa ON a.artist_id = aa.artist_id
+LEFT JOIN ARTIST_ALBUM aa ON a.artist_id = aa.fk_ALBUM_album_id
 GROUP BY a.artist_id, a.artist_name;
 
-SELECT * FROM ArtistAlbumCount;
+-- SELECT * FROM ArtistAlbumCount;
 
--- View 2: Faixas mais populares por gênero
--- Permite entender quais faixas são mais populares dentro de cada gênero, ajudando em análises de tendências e preferências do público.
-CREATE VIEW TopTracksByGenre AS
-SELECT g.genre_name, t.track_name, t.popularity
-FROM TRACK t
-JOIN GENRE g ON t.genre_id = g.genre_id
-WHERE t.popularity >= (SELECT AVG(popularity) FROM TRACK) -- Ajuste para tornar a métrica dinâmica
-ORDER BY g.genre_name, t.popularity DESC;
-
-SELECT * FROM TopTracksByGenre;
-
--- View 3: Gravadoras com maior número de lançamentos
+-- View 2: Gravadoras com maior número de lançamentos
 -- Útil para identificar as gravadoras mais ativas no mercado e possíveis tendências baseadas nos lançamentos.
 CREATE VIEW TopLabels AS
-SELECT l.label_name, COUNT(al.album_id) AS total_releases
+SELECT l.label_name, COUNT(al.FK_LABEL_label_id) AS total_releases
 FROM LABEL l
-JOIN ALBUM al ON l.label_id = al.label_id
+JOIN ALBUM al ON l.label_id = al.FK_LABEL_label_id
 GROUP BY l.label_name
 ORDER BY total_releases DESC
 LIMIT 10;
 
 SELECT * FROM TopLabels;
 
--- View 4: Artistas mais produtivos (com mais faixas)
+-- View 3: Artistas mais produtivos (com mais faixas)
 -- Ajuda a encontrar os artistas mais ativos na produção de músicas, indicando sua presença no mercado e possíveis influências nas tendências.
 CREATE VIEW MostProductiveArtists AS
-SELECT a.artist_name, COUNT(at.track_id) AS total_tracks
+SELECT a.artist_name, COUNT(at.fk_TRACK_track_id) AS total_tracks
 FROM ARTIST a
-JOIN ARTIST_TRACK at ON a.artist_id = at.artist_id
+JOIN ARTIST_TRACK at ON a.artist_id = at.fk_ARTIST_artist_id
 GROUP BY a.artist_name
 ORDER BY total_tracks DESC
 LIMIT 10;
 
 SELECT * FROM MostProductiveArtists;
-
