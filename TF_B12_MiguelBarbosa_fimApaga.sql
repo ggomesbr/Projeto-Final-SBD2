65078e4d37269c8fce6b702453aaad178db60974
Miguel de Frias <miguelfrias8@gmail.com>
2025-02-17 22:03:46 -0300

    add apaga

---
+++ TF_B12_MiguelBarbosa_fimApaga.sql
@@ -0,0 +1,23 @@
+-- -------- < Trabalho Final - Data Mining > --------
+--
+--                    SCRIPT DE REMOÇÃO (DDL)
+--
+-- Data Criação ...........: 16/02/2025
+-- Autor(es) ..............: Miguel Barbosa
+--                           Gabriel Gomes
+--                           Arthur Trindade
+-- Banco de Dados .........: MySQL 8.0
+-- Base de Dados (nome) ...: beatport
+--
+-- PROJETO => 01 Base de Dados
+--         => 07 Tabelas
+-- ----------------------------------------------------------------------------------------
+USE beatport;
+
+DROP TABLE ARTIST_TRACK;
+DROP TABLE ARTIST_ALBUM;
+DROP TABLE TRACK;
+DROP TABLE ALBUM;
+DROP TABLE LABEL;
+DROP TABLE GENRE;
+DROP TABLE ARTIST;
